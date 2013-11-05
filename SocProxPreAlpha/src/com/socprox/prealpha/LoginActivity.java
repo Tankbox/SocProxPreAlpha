7b662b5e1ba1cd5bf7ac0164db86a00f05202e54
Anthony Fammartino <afammart@gmail.com>
2013-11-04 22:48:26 -0800

    Login call made

--- SocProxPreAlpha/src/com/socprox/prealpha/LoginActivity.java
+++ SocProxPreAlpha/src/com/socprox/prealpha/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.socprox.prealpha;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -24,7 +27,24 @@ public class LoginActivity extends Activity {
 	public void loginButtonClicked(View view) {
 		String username = ((EditText)findViewById(R.id.usernameEditText)).getText().toString().trim();
 		String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString().trim();
-		
-		// Need to execute username and password for REST call now
+		try
+		{
+			String call = RESTCaller.loginCall(null, "socprox", username, password);
+	    	RESTCaller caller = new RESTCaller();
+	    	JSONObject result = caller.execute(call);
+	    	
+	    	if (result.get("success").equals(0))
+	    	{
+	    		
+	    	}
+	    	else
+	    	{
+	    		
+	    	}
+		} catch(Exception e)
+		{
+			System.out.print(e);
+			// Need to execute username and password for REST call now
+		}
 	}
 }
