eb255738c7b19ce3f4486c7338199640a469a38a
Austin Rowley <ao.rowley@gmail.com>
2013-11-12 14:06:26 -0700

    Adding a new project "SocProxNew"
    I took out the navigation drawer and made it a simple drop down menu.
    Also a new LoginActivity.

---
+++ SocProxNew/src/com/socprox/socproxnew/ChallengeFragment.java
@@ -0,0 +1,31 @@
+package com.socprox.socproxnew;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ChallengeFragment extends Fragment {
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	public ChallengeFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_dashboard_dummy,
+				container, false);
+		TextView dummyTextView = (TextView) rootView
+				.findViewById(R.id.section_label);
+		dummyTextView.setText(Integer.toString(getArguments().getInt(
+				ARG_SECTION_NUMBER)));
+		return rootView;
+	}
+}
