933a7945c6ec2d3125db922ae945a55acb759c8e
Austin Rowley <ao.rowley@gmail.com>
2013-11-14 14:59:00 -0700

    practically finshed with the interface for the new login screen

--- SocProxNew/gen/com/socprox/socproxnew/R.java
+++ SocProxNew/gen/com/socprox/socproxnew/R.java
@@ -10,6 +10,26 @@ package com.socprox.socproxnew;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f040009;
+        public static final int black=0x7f04000f;
+        public static final int blue=0x7f04000d;
+        public static final int bluegame=0x7f040010;
+        public static final int fuchsia=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int graygame=0x7f040011;
+        public static final int green=0x7f04000c;
+        public static final int lime=0x7f04000a;
+        public static final int maroon=0x7f040008;
+        public static final int navy=0x7f04000e;
+        public static final int olive=0x7f040006;
+        public static final int purple=0x7f040007;
+        public static final int red=0x7f040003;
+        public static final int silver=0x7f040004;
+        public static final int teal=0x7f04000b;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,8 +37,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int fconnect=0x7f020000;
@@ -26,15 +46,15 @@ public final class R {
         public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int btn_create_account=0x7f080007;
-        public static final int btn_fblogin=0x7f080003;
-        public static final int btn_login=0x7f080006;
-        public static final int container=0x7f080000;
-        public static final int img_logo=0x7f080002;
-        public static final int password=0x7f080005;
-        public static final int section_label=0x7f080001;
-        public static final int user_name=0x7f080004;
+        public static final int action_settings=0x7f090008;
+        public static final int btn_fblogin=0x7f090003;
+        public static final int btn_login=0x7f090006;
+        public static final int btn_signup=0x7f090007;
+        public static final int container=0x7f090000;
+        public static final int imageView1=0x7f090002;
+        public static final int password=0x7f090005;
+        public static final int section_label=0x7f090001;
+        public static final int user_name=0x7f090004;
     }
     public static final class layout {
         public static final int activity_dashboard=0x7f030000;
@@ -42,18 +62,19 @@ public final class R {
         public static final int login=0x7f030002;
     }
     public static final class menu {
-        public static final int dashboard=0x7f070000;
+        public static final int dashboard=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int desc=0x7f050004;
-        public static final int login=0x7f050007;
-        public static final int password_hint=0x7f050005;
-        public static final int sign_up=0x7f050008;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
-        public static final int user_name_hint=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int desc_fbutton=0x7f060004;
+        public static final int desc_logo=0x7f060009;
+        public static final int login=0x7f060007;
+        public static final int password_hint=0x7f060005;
+        public static final int sign_up=0x7f060008;
+        public static final int title_section1=0x7f060002;
+        public static final int title_section2=0x7f060003;
+        public static final int user_name_hint=0x7f060006;
     }
     public static final class style {
         /** 
@@ -77,10 +98,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
